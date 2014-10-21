a19c6ed700b828fa293c7f06ea52fdce56a2cd8b
宝码 <nongyoubao@alibaba-inc.com>
2014-10-21 15:28:49 +0800

    x

--- config.rb
+++ config.rb
@@ -1,3 +1,3 @@
-images_dir = './'
+images_dir = 'sassDir'
 line_comments = false
 output_style = (environment == :development) ? :expanded : :compressed
--- gulpfile.js
+++ gulpfile.js
@@ -11,42 +11,42 @@ var map = require('map-stream')
 
 gulp.task('del', function() {
   return gulp
-    .src(['sass/**/*.css', './build/css']) // 需要删除生成后的文件. gulp 不能覆盖
+    .src(['sass/**/*.css', './sassBuildTemp']) // 需要删除生成后的文件. gulp 不能覆盖
     .pipe(clean())
 })
 
 gulp.task('sass', ['del'], function() {
-  return gulp.src('./sass/style/*.scss')
+  return gulp.src('./sassDir/**/*.scss')
     .pipe(compass({
       config_file: path.join(process.cwd(), 'config.rb'),
-      project: path.join(process.cwd(), '/sass'),
-      css: './',
-      sass: 'style',
-      import_path: './',
+      project: process.cwd(),
+      css: 'sassBuildTemp',
+      sass: 'sassDir',
+      import_path: './sassDir',
       style: 'nested' //nested, expanded, compact, or compressed.
     }))
     .on('end', function() {
       console.log('compass 编译完成')
     })
-    .pipe(map(function(file, done){
+    .pipe(map(function(file, done) {
       console.log(file.path)
       done(null, file)
     }))
-    .pipe(gulp.dest('hehe'))
+
 });
 
 
-gulp.task('build', ['sass'], function() {
-  var cssfilter = filter('**/*.css')
-  return gulp
-    .src(['sass/*.css', 'sass/*/*.png'])
-    .pipe(cssfilter)
-    .pipe(minifyCSS({
-      keepSpecialComments: 0,
-      keepBreaks: false
-    }))
-    .pipe(cssfilter.restore())
-    .pipe(copy('./build/css', {
-      'prefix': 1
-    }))
-})
+// gulp.task('build', ['sass'], function() {
+//   var cssfilter = filter('**/*.css')
+//   return gulp
+//     .src(['sass/*.css', 'sass/*/*.png'])
+//     .pipe(cssfilter)
+//     .pipe(minifyCSS({
+//       keepSpecialComments: 0,
+//       keepBreaks: false
+//     }))
+//     .pipe(cssfilter.restore())
+//     .pipe(copy('./build/css', {
+//       'prefix': 1
+//     }))
+// })
